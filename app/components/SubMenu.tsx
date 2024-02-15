2c186246d79063e0fb0f1bd21251439b1f136266
ahmetalisarihan <105447763+ahmetalisarihan@users.noreply.github.com>
2024-02-15 23:54:32 +0300

    Update

---
+++ app/components/SubMenu.tsx
@@ -0,0 +1,92 @@
+export const Menu = [
+    {
+        id: 1,
+        name: 'Plastik Boru',
+        path: '/plastikborular',
+        submenu: [
+            {
+                id: 1,
+                name: 'PVC Boru',
+                path: '/pvc'
+            },
+            {
+                id: 2,
+                name: 'PE Boru',
+                path: '/pe'
+            },
+            {
+                id: 3,
+                name: 'PP Boru',
+                path: '/pp'
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: 'Çelik Borular',
+        path: '/celikborular',
+        submenu: [
+            {
+                id: 1,
+                name: 'Dikisiz Boru',
+                path: '/dikisiz'
+            },
+            {
+                id: 2,
+                name: 'Dikili Boru',
+                path: '/dikili'
+            },
+            {
+                id: 3,
+                name: 'Dikisli Boru',
+                path: '/dikisli'
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: 'Vanalar',
+        path: '/vanalar',
+        submenu: [
+            {
+                id: 1,
+                name: 'Küresel Vana',
+                path: '/kuresel'
+            },
+            {
+                id: 2,
+                name: 'Kelebek Vana',
+                path: '/kelebek'
+            },
+            {
+                id: 3,
+                name: 'Çek Vana',
+                path: '/cek'
+            }
+        ]
+
+    },
+    {
+        id: 4,
+        name: 'Kelepçeler',
+        path: '/kelepceler',
+        submenu: [
+            {
+                id: 1,
+                name: 'PVC Kelepçe',
+                path: '/pvc'
+            },
+            {
+                id: 2,
+                name: 'Çelik Kelepçe',
+                path: '/celik'
+            },
+            {
+                id: 3,
+                name: 'Galvaniz Kelepçe',
+                path: '/galvaniz'
+            }
+        ]
+    }
+
+]

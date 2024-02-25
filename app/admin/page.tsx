ff08f04371777cb45c143869be4f29b313323132
ahmetalisarihan <105447763+ahmetalisarihan@users.noreply.github.com>
2024-02-25 23:33:14 +0300

    update

---
+++ app/admin/page.tsx
@@ -0,0 +1,12 @@
+import UploadImage from '@/components/UploadImage'
+import React from 'react'
+
+const AdminPage = () => {
+  return (
+    <div>
+      <UploadImage />
+    </div>
+  )
+}
+
+export default AdminPage
---
+++ app/api/upload-image/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+
+
+export const POST =async (req:NextRequest) => {
+    const formData = await req.formData()
+
+    const image = formData.get('image') as unknown as File
+
+    console.log({image})
+
+    return NextResponse.json({msg: image}, { status:200})
+    
+}

2ce0d8b1caf54479b7807e8a2e65865297f8906c
ahmetalisarihan <105447763+ahmetalisarihan@users.noreply.github.com>
2024-04-04 23:54:47 +0300

    Add Banner

---
+++ components/Banner.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Banner = () => {
+  return (
+<div className="bg-sky-600 text-white p-1 flex justify-between items-center">
+      <div className="flex items-center">
+        {/* <img src="/indir indirimi.jpg" alt="İndirim" className="w-6 h-6 mr-4" /> */}
+        <h3 className="font-bold text-lg pl-2">Avantajlı Fiyatlarımızdan Faydalanmak İçin Teklif Alın</h3>
+      </div>
+      <Link href="/contact" className="bg-white text-blue-500 px-4 py-1 rounded-md font-bold">
+        Hemen Teklif Alın
+      </Link>
+    </div>
+  )
+}
+
+export default Banner

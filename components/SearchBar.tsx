b2e7b5f91b14dda63fc6b523b208371beadab12f
ahmetalisarihan <105447763+ahmetalisarihan@users.noreply.github.com>
2024-04-16 14:57:05 +0300

    add Search

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,31 +1,52 @@
 'use client'
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useState } from "react";
-import { TProduct } from "@/app/types";
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import SearchResults from '@/components/SearchResults';
+import { TProduct } from '@/app/types';
 
 interface SearchBarProps {
   onSearch: (products: TProduct[]) => void;
 }
 
-export default function SearchBar({ onSearch }: SearchBarProps) {
-  const [query, setQuery] = useState("");
-  // const router = useRouter();
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState<TProduct[]>([]);
+  const [showResults, setShowResults] = useState(false); // Sonuçları göstermek için state
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      if (query.length >= 3) {
+        try {
+          const response = await fetch(`/api/search?query=${query}`);
+          const products = await response.json();
+          console.log(products)
+          setResults(products);
+          setShowResults(true); // Sonuçlar geldiğinde sonuçları göster
+        } catch (error) {
+          console.error('Ürünler alınırken bir hata oluştu:', error);
+          setShowResults(false); // Hata durumunda sonuçları gizle
+        }
+      } else {
+        setResults([]);
+        setShowResults(false); // Sonuç yoksa sonuçları gizle
+      }
+    };
+
+    fetchResults();
+  }, [query]);
 
   const handleSearch = async () => {
-    try {
-      const response = await fetch(`/api/search?query=${query}`);
-      const products = await response.json();
-      onSearch(products);
-      // router.push(`/search?query=${query}`);
-    } catch (error) {
-      console.error("Ürünler alınırken bir hata oluştu:", error);
-    }
+    onSearch(results);
+    setShowResults(false); // Arama yapıldığında sonuçları gizle
+    router.push(`/search?query=${query}`);
   };
 
   return (
-    <div className="flex items-center space-x-2">
+    <div className="flex flex-col items-center space-x-2 relative"> {/* Popover için relative pozisyon */}
+    <div className='flex'>
       <Input
         type="text"
         className="px-3 py-2 w-80"
@@ -33,9 +54,12 @@ export default function SearchBar({ onSearch }: SearchBarProps) {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
-      <Button className="px-3 py-2" onClick={handleSearch}>
-        Ara
-      </Button>
+      <Button onClick={handleSearch}>Ara</Button></div>
+      
+      {/* SearchResults bileşenini burada çağır ve showResults prop'unu geçir */}
+      <SearchResults results={results} />
     </div>
   );
-}
+};
+
+export default SearchBar;
---
+++ components/SearchResults.tsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { TProduct } from '@/app/types';
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import Link from 'next/link';
+
+interface SearchResultsProps {
+  results: TProduct[];
+}
+
+const SearchResults: React.FC<SearchResultsProps> = ({ results }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setIsOpen(results.length > 0);
+  }, [results]);
+
+  return (
+    <Popover open={isOpen} onOpenChange={setIsOpen}>
+      <PopoverTrigger></PopoverTrigger>
+      {isOpen && (
+        <PopoverContent className="w-80">
+          {results.map((result) => (
+            <Link key={result.id} href={`/products/${result.id}`}>
+              <div className="mb-2 hover:bg-gray-200 cursor-pointer">
+                {result.name}
+              </div>
+            </Link>
+          ))}
+        </PopoverContent>
+      )}
+    </Popover>
+  );
+};
+
+export default SearchResults;
